68caca618ccd79121e8b4df1b00ba5d15c8c3a0e
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-09-10 18:27:25 +0500

    manual url enter redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :redirect_if_logged_in
   include Pundit::Authorization
   include Authentication
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
   def pundit_user
@@ -13,6 +14,10 @@ class ApplicationController < ActionController::Base
     if session[:user_id].present?
       redirect_to home_path, alert: "You are already logged in"
     end
+  end
+  def user_not_authorized
+    flash[:error] = "You are not authorized to perform this action"
+    redirect_to root_path
   end
   # Changes to the importmap will invalidate the etag for HTML responses
   stale_when_importmap_changes
