626b10d46748b3e39a656487f7104f77117f426a
umerTiksom <muhammad.umer@tiksom.co.uk>
2026-09-14 15:35:52 +0500

    make a unique project for the user

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,6 @@
 class Project < ApplicationRecord
   belongs_to :user
   has_many :tasks, dependent: :destroy, foreign_key: :projects_id
-  validates :name, presence: true, length:{minimum:5,maximum: 50}
+  validates :name, presence: true, length:{minimum:5,maximum: 50}, uniqueness: {scope: :user_id, message: "project must be unique for the owner"}
   validates :description, presence: true, length:{minimum:5,maximum: 500}
 end
